c7f9b17dec86be9d0da4a29c37faf971d845534a
adriancocoara <adriancocoara336@gmail.com>
2018-12-02 17:36:17 +0200

    hangman app

---
+++ scripts/requests.js
@@ -0,0 +1,20 @@
+const getPuzzle = async (wordCount) => {
+    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    
+    if (response.status === 200) {
+        const data = await response.json()
+        return data.puzzle
+    } else {
+        throw new Error('Unable to get puzzle')
+    }
+}
+
+const getLocation = () => {
+    return fetch('http://ipinfo.io/json?token=29d7723969f9a5').then((response) => {
+        if (response.status === 200) {
+            return response.json()
+        } else {
+            throw new Error('Unable to fetch location')
+        }
+    })
+}
